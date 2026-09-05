719982a38381a496e470782f4dfcd1cb4d61f836
Leonid Kogan <leonsio@users.noreply.github.com>
2026-09-05 20:37:26 +0200

    Add configurable calendar event types

--- custom_components/sph/const.py
+++ custom_components/sph/const.py
@@ -5,7 +5,9 @@ CONF_PASSWORD = "password"
 CONF_CHILD_NAME = "child_name"
 CONF_CHILD_SHORTCUT = "child_shortcut"
 CONF_UPDATE_INTERVAL = "update_interval"
+CONF_CALENDAR_EVENT_TYPES = "calendar_event_types"
 DEFAULT_UPDATE_INTERVAL = 60
+DEFAULT_CALENDAR_EVENT_TYPES = ["Arbeiten", "Klausuren"]
 
 # Authentication is hosted separately from the legacy school portal.
 SPH_BASE = "https://start.schulportal.hessen.de"
