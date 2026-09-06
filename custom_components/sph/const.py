f81d1b8942d5057641d2e300c24c5cc3430c1d14
Leonid Kogan <leonsio@users.noreply.github.com>
2026-09-06 12:19:04 +0200

    Add configurable module enable flags

--- custom_components/sph/const.py
+++ custom_components/sph/const.py
@@ -6,8 +6,13 @@ CONF_CHILD_NAME = "child_name"
 CONF_CHILD_SHORTCUT = "child_shortcut"
 CONF_UPDATE_INTERVAL = "update_interval"
 CONF_CALENDAR_EVENT_TYPES = "calendar_event_types"
+CONF_MODULE_STUNDENPLAN = "module_stundenplan"
+CONF_MODULE_KALENDER = "module_kalender"
+CONF_MODULE_MEINUNTERRICHT = "module_meinunterricht"
+CONF_MODULE_LERNGRUPPEN = "module_lerngruppen"
 DEFAULT_UPDATE_INTERVAL = 60
 DEFAULT_CALENDAR_EVENT_TYPES = ["Arbeiten", "Klausuren"]
+DEFAULT_MODULE_ENABLED = True
 
 # Authentication is hosted separately from the legacy school portal.
 SPH_BASE = "https://start.schulportal.hessen.de"
